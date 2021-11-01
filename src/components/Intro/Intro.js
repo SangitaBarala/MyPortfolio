68b32f1a2184b48cf2613f40ee0d48bb0e5baa8d
Sangita <sangitakhatkar.0425@gmail.com>
2021-10-31 21:36:17 -0400

    changes

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useRef} from "react";
 import './Intro.scss'
+import Resume from '../Resume/Sangita_Barala_-_ReactWeb_Developer (1).ef369f89 (1).pdf'
+
 import {init} from 'ityped'
 const Intro = () => {
     const textRef = useRef()
@@ -22,9 +24,11 @@ const Intro = () => {
                     <h4>I love coding , and I aspire to become a great developer.
                         I currently code every day to become better than previous day!</h4>
                     <div>
-                        <a href="https://github.com" className="fa fa-github" target="_blank"></a>
+                        <a href="https://github.com/SangitaBarala" className="fa fa-github" target="_blank"></a>
                         <a href="https://www.linkedin.com/in/sangita-barala-854854205/" target="_blank" className="fa fa-linkedin"></a>
-                        <a href="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=f6bffc90ab&attid=0.1&permmsgid=msg-a:r-2595450270934863741&th=17c7aea0f2acbfb0&view=att&disp=inline&realattid=f_kupujuva0&saddbat=ANGjdJ_-l1TNUS4ymlXcAjVY67Wlz44ZVFL-5yVXL-OY3hwWVWjZY-iFwh9Ulp2cUySUSd8Y1DpgeQVHyY6ci4nIpUniJMjMGUn6mVB0-2Rald2EDWJEmtr-I76W-s5BywR1oKtJHca9cItLLmY6TWyiY5Bf7mAvRxT-sF-CFg5hrpOa2EXFQCJ3UdaoaPEG6lteUj8sbZ3ZZkjrErh7yoy-qUqEVoChrnazMT9yUn9JZZqsAOp-BuU7nQXv0oaWoBZ5X3357GvyzAu4v6IKl112wbR9QrIryHG3IGvzsbdp1Hir0ZuhDfU8VPjUJH6E6gh2nVAESXeo0tpRGsj-gyBT6Rta_l6l0aDm5AK06aYpmPPn6rBlJEd5jdmg6X870VjmcGn4vCEgcP854hF2bf9zgmYPIMTUJ_kjdlkWNcpNljl11h3QWv8_vxoWQEQeCF3KSz0iKCACdXmxT417JWRAHfLd8zicywa8ot1Y3JnS0xEJKeypOV24pt-CKxu4MXcQfqdYSrnB5PVRy2ms1wOSruxMF6k1XfjFRm4J66_3PzET80SnvE07zeQQ2u6zVQNkIZXERibkDVG7MhTGoBsa1OC5aAlRVLD30bSU2zo3-AXUMniIm_bZMpjYqRbWFtQRqN7R01g5UBf_2xt0vjuWGXGV75ZGBzwMnVGJxtlUUULQTh6u7b4PlG4yl2c" target="_blank" className="fa fa-folder"></a>
+                        <a href={Resume} target="_blank" className="fa fa-folder">
+                            <span className="resume_text">Download Resume</span>
+                        </a>
                     </div>
                 </div>
             </div>
